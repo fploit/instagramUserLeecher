79bf853fedfbcbd354e2a33af8a4d70c30e0bc33
jilrot <53963715+jilrot@users.noreply.github.com>
2019-10-24 22:22:22 +0330

    Add files via upload

---
+++ leecher.py
@@ -0,0 +1,54 @@
+from instabot import Bot
+import os, getpass
+
+logo = '''
+\033[0;35m
+            ──▄█████████████████████████▄──
+            ▄█▀░█░█░█░░░░░░░░░░░░░░░░░░░▀█▄
+            █░░░█░█░█░░░░░░░░░░░░░░█████░░█
+            █░░░█░█░█░░░░░░░░░░░░░░█████░░█
+            █░░░░░░░░░▄▄▄█████▄▄▄░░░░░░░░░█
+            ███████████▀▀░░░░░▀▀███████████
+            █░░░░░░░██░░▄█████▄░░██░░░░░░░█
+            █░░░░░░░██░██▀░░░▀██░██░░░░░░░█
+            █░░░░░░░██░██░░░░░██░██░░░░░░░█
+            █░░░░░░░██░██▄░░░▄██░██░░░░░░░█
+            █░░░░░░░██▄░▀█████▀░▄██░░░░░░░█
+            █░░░░░░░░▀██▄▄░░░▄▄██▀░░░░░░░░█
+            █░░░░░░░░░░▀▀█████▀▀░░░░░░░░░░█
+            █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+            ▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░▄█▀
+            ──▀█████████████████████████▀──
+\033[0;34m
+           +---  Instagram User Leecher ---+ \033[0;32m
+           https://jilrot.com
+           t.me/jailbreakandroot
+
+
+  ### Login ###
+
+'''
+
+os.system("clear")
+print (logo)
+
+user = input("Username: ")
+passwd = getpass.getpass("Password: ")
+print
+account = input("Account to extract user: ")
+
+bot = Bot()
+bot.login(username=user, password=passwd)
+user_id = bot.get_user_id_from_username(account)
+followers = bot.get_user_followers(user_id)
+
+f = open("usernames.lst", "w")
+
+for user_id in followers:
+    username = bot.get_username_from_user_id(user_id)
+    print (username)
+    f.write(username+'\n')
+
+f.close()
+
+os.system("rm -rf *log *txt config *json mrsploit.checkpoint")
